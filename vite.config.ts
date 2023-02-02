f26d2de155c4bce95a15f34f9d686a7163d84611
bananvyhe <loadonden@yahoo.com>
2023-02-03 02:53:29 +0500

    vite full reload plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import FullReload from 'vite-plugin-full-reload'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
+  	FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
     RubyPlugin(),
     vue()
   ],
