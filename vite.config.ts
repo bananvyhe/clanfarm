63cd2248b322731c8d583a58aa151d277ab08d72
bananvyhe <loadonden@yahoo.com>
2023-02-03 21:58:26 +0500

    vite setup. all works well

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import FullReload from 'vite-plugin-full-reload'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+ 
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
   plugins: [
-  	FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
+  	FullReload(['config/routes.rb', 'app/views/**/*', 'app/frontend/**/*'], { delay: 200 }),
     RubyPlugin(),
     vue()
   ],
