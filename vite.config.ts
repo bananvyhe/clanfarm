d04f1190a1bd90c5af165ab6622eac57cfa03203
bananvyhe <loadonden@yahoo.com>
2023-02-12 00:43:52 +0500

    rollup visualizer plug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import FullReload from 'vite-plugin-full-reload'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { visualizer } from "rollup-plugin-visualizer"
 export default defineConfig({
  
   server: {
@@ -11,9 +12,13 @@ export default defineConfig({
     }
   },
   plugins: [
+    visualizer(),
   	FullReload(['config/routes.rb', 'app/views/**/*', 'app/frontend/**/*'], { delay: 200 }),
     RubyPlugin(),
     vue(),
-    vuetify({ autoImport: true }), // Enabled by default
+    vuetify({ 
+      autoImport: true,
+      // configFile: 'src/settings.scss'
+    }), // Enabled by default
   ],
 })
