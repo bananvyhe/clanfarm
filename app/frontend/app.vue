dcedcb1384e9c75bc45d5bf2becb537aec42f4b3
bananvyhe <loadonden@yahoo.com>
2023-02-16 17:32:06 +0500

    usescroll sensor add

--- app/frontend/app.vue
+++ app/frontend/app.vue
@@ -1,13 +1,29 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <v-btn>tswwss</v-btn>
+  <div>
+		<v-layout >
+			<v-app-bar density="compact" class="py-0">
+				{{arrivedState.bottom }} {{isScrolling }}
+			</v-app-bar>
+				123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>	
+				<div v-if="bottom">Uhuy! Sampai bawah</div>	
+		</v-layout>
   </div>
 </template>
+<script setup lang="ts">
+	import { ref, computed } from 'vue'
+	import { useScroll } from '@vueuse/core'
+	// const content = ref(<HTMLElement | null>(null))
+	// const { arrivedState } = useScroll(content)
+	const { x, y, isScrolling, arrivedState, directions } = useScroll(document)
 
-<script setup>
-  import {ref} from 'vue'
-  const message = ref('hello, composition API')
+	const bottom = computed(() => {
+	  if (arrivedState.bottom) {
+	    console.log('Yay!')
+	    console.log( arrivedState.bottom )
+	    return true
+	  }
+	  return false
+	})
 </script>
 
 <style scoped>
