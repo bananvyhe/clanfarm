9cddf7328e778bb9c27710481ffbb38cd237bed9
bananvyhe <loadonden@yahoo.com>
2023-02-23 02:26:35 +0500

    hide on scroll menu with gsap
    in vuetify this should implemented in 3.2

--- app/frontend/app.vue
+++ app/frontend/app.vue
@@ -1,29 +1,82 @@
 <template>
   <div>
 		<v-layout >
-			<v-app-bar density="compact" class="py-0">
-				{{arrivedState.bottom }} {{isScrolling }}
+			<v-app-bar density="compact" class="topmenu py-0">
+				{{arrivedState.bottom }} {{isScrolling }} 
 			</v-app-bar>
-				123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>	
-				<div v-if="bottom">Uhuy! Sampai bawah</div>	
+			<!-- <v-btn @click = "topmenu"></v-btn> -->
+				123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123
+<!-- 				<br>123directions.bottom {{directions.bottom}} <br>showmenutop.status {{showmenutop.status}}<br> isScrolling {{isScrolling }} <br>directions.top {{directions.top}}<br>toBottom {{toBottom}}<br>toTop {{toTop
+				}}<br> -->
+				123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br><br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br><br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>123<br>
+				<!-- <div v-if="bottom">Uhuy! Sampai bawah</div>	 -->
 		</v-layout>
   </div>
 </template>
 <script setup lang="ts">
-	import { ref, computed } from 'vue'
-	import { useScroll } from '@vueuse/core'
-	// const content = ref(<HTMLElement | null>(null))
-	// const { arrivedState } = useScroll(content)
-	const { x, y, isScrolling, arrivedState, directions } = useScroll(document)
+import { gsap } from "gsap";
+import { ref, computed, watch, reactive, onMounted, onBeforeMount, toRefs } from 'vue'
+import { useScroll } from '@vueuse/core'
+import { debounce } from 'lodash'
+const { x, y, isScrolling, arrivedState, directions } = useScroll(document)
+const { left: toLeft, right: toRight, top: toTop, bottom: toBottom } = toRefs(directions)
 
-	const bottom = computed(() => {
-	  if (arrivedState.bottom) {
-	    console.log('Yay!')
-	    console.log( arrivedState.bottom )
-	    return true
-	  }
-	  return false
-	})
+onMounted(() => {
+
+})
+
+//  
+
+const m1 = gsap.timeline();
+
+let isFirstUpdate = true
+const showmenutop = reactive({ status: true});
+
+const debouncedFunc = debounce(() => {
+  m1.to(".topmenu",{
+    duration: 0.5,
+    y: 0,
+    ease: "elastic.in",
+  });
+  showmenutop.status = true;
+  console.log("scroll top");
+}, 500);
+
+watch(() => toTop.value, 
+	(newVal, oldVal) =>{
+		console.log(newVal)
+		console.log(oldVal)
+		if (isFirstUpdate) {
+      isFirstUpdate = false
+    } else {
+    	debouncedFunc();			 
+    }
+  },
+	{deep: true}
+)
+
+const debouncedFuncbot = debounce(() => {
+  m1.to(".topmenu",{
+  duration: 0.5,
+  y: -48,
+  ease: "elastic.in",
+})
+ showmenutop.status = false
+  console.log("scroll botom");
+}, 500);
+
+watch(() => toBottom.value, 
+	(newVal, oldVal) =>{
+		console.log(newVal)
+		console.log(oldVal)
+ 		if (isFirstUpdate) {
+      isFirstUpdate = false
+    } else {
+    	debouncedFuncbot();			 
+    }
+	}, 
+	{deep: true}
+)
 </script>
 
 <style scoped>
