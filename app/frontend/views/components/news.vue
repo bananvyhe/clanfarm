a9f299ca948d1291ad1b769025198c673d1f1742
bananvyhe <loadonden@yahoo.com>
2023-05-11 00:37:04 +0500

    prod settings

--- app/frontend/views/components/news.vue
+++ app/frontend/views/components/news.vue
@@ -6,7 +6,7 @@
           <div 
           class="align-top float-left mr-1 px-3 py-md-1 mx-md-1">
           <!-- v-bind:style="{backgroundImage: 'url('+ item.pic}" -->
-            <div class ="pic px-0 align-center my-2"  >
+            <div class ="pic px-0 align-center my-2" v-bind:style="{backgroundImage: 'url('+ item.pic}">
               <!-- {{pos}} --> 
             </div>
           </div >
--- app/frontend/views/components/pumpk.vue
+++ app/frontend/views/components/pumpk.vue
@@ -92,8 +92,8 @@
   } 
 
   function hitpumpk(){
-       // var interval = 15000000;
-    var interval = 26500;
+       var interval = 15000000;
+    // var interval = 26500;
     // var interval = 7000;
     hitcalc()
     dmg.value =  damageData.hit 
