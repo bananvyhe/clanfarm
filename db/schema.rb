9e29841f827f6f6aedb710af726f747a2f210c20
bananvyhe <loadonden@yahoo.com>
2023-05-27 14:40:21 +0500

    add combat points field to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_172703) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_27_093909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_172703) do
     t.boolean "dead", default: false, null: false
     t.integer "pk", default: 0, null: false
     t.integer "cry", default: 0, null: false
+    t.integer "cpoints", default: 0, null: false
   end
 
   add_foreign_key "dropitems", "listitems"
