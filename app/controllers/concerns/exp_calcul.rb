7748ad720d734c48c96f7fdc9ed3d426a3a54b78
bananvyhe <loadonden@yahoo.com>
2023-06-15 19:12:19 +0500

    exp calcul on server

--- app/controllers/concerns/exp_calcul.rb
+++ app/controllers/concerns/exp_calcul.rb
@@ -1,7 +1,52 @@
 module ExpCalcul
 
-  def calcul_getexp(param)
- 
+  def calcul_getexp(exp)
+  level_data = [
+    [68, 1],
+    [369, 2],
+    [1168, 3],
+    [2884, 4],
+    [6038, 5],
+    [11287, 6],
+    [19423, 7],
+    [31378, 8],
+    [48229, 9],
+    [71202, 10],
+    [101677, 11],
+    [141193, 12],
+    [191454, 13],
+    [254330, 14],
+    [331867, 15],
+    [426288, 16],
+    [540000, 17],
+    [675596, 18],
+    [835862, 19],
+    [920357, 20],
+    [1015431, 21],
+    [1123336, 22],
+    [1246808, 23],
+    [1389235, 24],
+    [1554904, 25],
+    [1749413, 26],
+    [1980499, 27],
+    [2260321, 28],
+    [2634751, 29],
+    [2844287, 30],
+    [3093068, 31],
+    [3389496, 32],
+    [3744042, 33],
+    [3744042, 33]
+  ]
+    level_data.each_with_index do |data, i|
+      if exp <= data[0]
+        previous_exp = i > 0 ? level_data[i - 1][0] : 0
+        current_exp = data[0]
+        progress = (exp - previous_exp).to_f / (current_exp - previous_exp) * 100
+        return [i + 1, progress]
+      end
+    end
+
+  nil # Experience exceeds maximum level 
   end
  
 end
--- app/controllers/mobs_controller.rb
+++ app/controllers/mobs_controller.rb
@@ -1,8 +1,19 @@
 class MobsController < ApplicationController
 	before_action :authorize_access_request!
+	include ExpCalcul 
 
 	def hitghoul
-		# hitghoul = Mob.find_by!(name: "ghoul")
+		userfind = User.find(payload['user_id'])
+		expa = userfind.expirience.to_i
+		level_info = calcul_getexp(expa)
+		if level_info
+		  level = level_info[0]
+		  progress = level_info[1]
+		  puts "Player Level: #{level}"
+		  puts "Level Progress: #{progress.round(2)}%"
+		else
+		  puts "Experience exceeds maximum level"
+		end
 		mob = Mob.new
 		hit = mob.hitcalcul(150, 1)
 		puts hit
@@ -23,8 +34,9 @@ class MobsController < ApplicationController
 
 			exp = calculate_experience(@gho.mob.hp.to_i)	
 			# puts exp
-			userfind = User.find(payload['user_id'])
+			
 			userfind.expirience += exp
+			userfind.loa += loa
 			userfind.save
 		end
 
@@ -33,12 +45,63 @@ class MobsController < ApplicationController
 		if  exp
 			response['exp'] = exp
 		end
+
 		response['loa'] = loa
 		response['hit'] = hit
 		 
 		# puts @gho.inspect
 		render json: response
 	end
+
+# def calculate_level(exp)
+#   level_data = [
+#     [68, 1],
+#     [369, 2],
+#     [1168, 3],
+#     [2884, 4],
+#     [6038, 5],
+#     [11287, 6],
+#     [19423, 7],
+#     [31378, 8],
+#     [48229, 9],
+#     [71202, 10],
+#     [101677, 11],
+#     [141193, 12],
+#     [191454, 13],
+#     [254330, 14],
+#     [331867, 15],
+#     [426288, 16],
+#     [540000, 17],
+#     [675596, 18],
+#     [835862, 19],
+#     [920357, 20],
+#     [1015431, 21],
+#     [1123336, 22],
+#     [1246808, 23],
+#     [1389235, 24],
+#     [1554904, 25],
+#     [1749413, 26],
+#     [1980499, 27],
+#     [2260321, 28],
+#     [2634751, 29],
+#     [2844287, 30],
+#     [3093068, 31],
+#     [3389496, 32],
+#     [3744042, 33],
+#     [3744042, 33]
+#   ]
+
+#   level_data.each_with_index do |data, i|
+#     if exp <= data[0]
+#       previous_exp = i > 0 ? level_data[i - 1][0] : 0
+#       current_exp = data[0]
+#       progress = (exp - previous_exp) / (current_exp - previous_exp) * 100
+#       return [i + 1, progress]
+#     end
+#   end
+
+#   nil # Experience exceeds maximum level
+# end
 
 	def calculate_experience(health)
 	  min_health = 100 
