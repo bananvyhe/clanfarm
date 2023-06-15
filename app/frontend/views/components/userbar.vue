47d1da69a7f11a2157acef53ea9e29349dfb4741
bananvyhe <loadonden@yahoo.com>
2023-06-15 15:48:53 +0500

    lvl calcul based on existing exp

--- app/frontend/views/components/userbar.vue
+++ app/frontend/views/components/userbar.vue
@@ -2,7 +2,6 @@
   <div class="d-flex "> 
  
     <div v-if="store.tsignedIn == true" class="d-flex align-center">
-      {{store.texpirience}} 
       <v-btn
         color="primary"  
         @click="signOut">выйти
@@ -48,8 +47,8 @@
 <script setup lang="ts">
 const karma = ref(0)
 const userhp = ref(80)
-const lvl = ref(13)
-const exp = ref(50)
+ 
+
 
 const cp = ref(5)
 const avcp = ref(3)
@@ -81,6 +80,75 @@ import ls from 'localstorage-slim';
   // import { useLogStore } from 'store.js'
   import Signup from './sign_up.vue';
   import Signin from './sign_in.vue';
+  const lvl = ref()
+  const exp = ref()
+//вычисление уровня
+onMounted(() => {
+  nextTick(() => {
+    const levelInfo = calculateLevel(store.texpirience);
+
+    if (levelInfo) {
+      const level = levelInfo[0];
+      const progress = levelInfo[1];
+      lvl.value = level
+      exp.value = progress
+
+      // console.log("Player Experience:", playerExperience);
+      console.log("Player Level:", level);
+      console.log("Level Progress:", progress.toFixed(2) + "%");
+    } else {
+      console.log("Experience exceeds maximum level");
+    }
+  })
+})
+ 
+  function calculateLevel(exp) {
+    const levelData = [
+      [68, 1],
+      [369, 2],
+      [1168, 3],
+      [2884, 4],
+      [6038, 5],
+      [11287, 6],
+      [19423, 7],
+      [31378, 8],
+      [48229, 9],
+      [71202, 10],
+      [101677, 11],
+      [141193, 12],
+      [191454, 13],
+      [254330, 14],
+      [331867, 15],
+      [426288, 16],
+      [540000, 17],
+      [675596, 18],
+      [835862, 19],
+      [920357, 20],
+      [1015431, 21],
+      [1123336, 22],
+      [1246808, 23],
+      [1389235, 24],
+      [1554904, 25],
+      [1749413, 26],
+      [1980499, 27],
+      [2260321, 28],
+      [2634751, 29],
+      [2844287, 30],
+      [3093068, 31],
+      [3389496, 32],
+      [3744042, 33],
+      [3744042, 33]
+    ];
+
+    for (let i = 0; i < levelData.length; i++) {
+      if (exp <= levelData[i][0]) {
+        return [i + 1, (exp - levelData[i - 1][0]) / (levelData[i][0] - levelData[i - 1][0]) * 100];
+      }
+    }
+
+    return null; // Experience exceeds maximum level
+  }
+ 
   // import Signin from './packs/components/Signin.vue';
   // const signedIn = computed(() => store.tsignedIn)
 // const trock = computed(() => store.trock)
