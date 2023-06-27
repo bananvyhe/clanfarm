463dff56269eb6db3333ba1ff230dd50d79137a9
bananvyhe <loadonden@yahoo.com>
2023-06-27 23:55:15 +0500

    fix

--- app/workers/cp_worker.rb
+++ app/workers/cp_worker.rb
@@ -5,17 +5,17 @@ class CpWorker < ApplicationController
 		users.each do |c|
 			if c.dead == false && c.cpoints > c.avcpoints 
 				c.avcpoints += 2
-				puts c.avcpoints 
-				puts c.cpoints
-				cp = c.avcpoints-2
 				if c.avcpoints > c.cpoints && c.karma < 0
 					c.avcpoints = c.cpoints 
-				elsif cp > c.cpoints && c.karma >= 0
-					c.avcpoints = c.cpoints+2
+				end
+				c.save
+			elsif c.avcpoints >= c.cpoints && c.avcpoints < c.cpoints+2 &&  c.karma >= 0
+				c.avcpoints += 2
+				if c.avcpoints > c.cpoints+2  
+					c.avcpoints = c.cpoints+2 
 				end
 				c.save
 			end
-
 		end 
 	end
 end
