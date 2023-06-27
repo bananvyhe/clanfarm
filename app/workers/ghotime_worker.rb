07b377895ddf9517ed19f70443aa258ad2a99e36
bananvyhe <loadonden@yahoo.com>
2023-06-28 01:39:38 +0500

    ghoul res worker

---
+++ app/workers/ghotime_worker.rb
@@ -0,0 +1,19 @@
+class GhotimeWorker < ApplicationController
+	include Sidekiq::Worker
+	require 'time'
+	def perform
+		eight_hours_ago = Time.now - (180)
+	 	gho = MobUser.all
+	 	.joins(:mob).where('name = ?', 'ghoul' )
+ 		gho.each do |c|
+ 			if c.death == true
+ 				if eight_hours_ago > c.updated_at 
+ 					puts "time"
+ 					c.death = false
+ 					c.damagedeal = 0
+ 					c.save
+ 				end
+ 			end
+ 		end
+	end
+end
