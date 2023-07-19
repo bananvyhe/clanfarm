3b1b6733ae9f83ab661d6b2b45d8c7b72b5321b9
bananvyhe <loadonden@yahoo.com>
2023-07-20 03:29:19 +0500

    1

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -2,7 +2,7 @@
 JWTSessions.encryption_key = Rails.application.secret_key_base
 JWTSessions.token_store = :redis, { 
 	redis_url: "redis://127.0.0.1:6379",
-	pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 15))
+	pool_size: Integer(ENV.fetch("RAILS_MAX_THREADS", 25))
 }
 JWTSessions.access_exp_time = 1209600
 # JWTSessions.access_exp_time = 20
