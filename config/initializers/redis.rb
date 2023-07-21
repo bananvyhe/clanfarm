c0b56ff111e6a3908ed92c502141ce9a90f82ade
bananvyhe <loadonden@yahoo.com>
2023-07-21 14:19:25 +0500

    try set pollsize for redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,13 @@
+# config/initializers/redis.rb
+
+# Assuming you are using the default Redis URL (redis://localhost:6379/0)
+# If you have a different Redis configuration, adjust accordingly.
+
+redis_config = {
+  url: ENV['REDIS_URL'], # Set your Redis URL here, e.g., 'redis://localhost:6379/0'
+  pool_size: 30 # Set your desired connection pool size here
+}
+
+Redis.current = ConnectionPool.new(size: redis_config[:pool_size]) do
+  Redis.new(url: redis_config[:url])
+end
