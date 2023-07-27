0352d8a44d39893f688f4b72d988574e879e47d7
bananvyhe <loadonden@yahoo.com>
2023-07-28 02:03:36 +0500

    1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,14 @@
 
-Sidekiq::Extensions.enable_delay!
-Sidekiq.configure_server do |config|
- config.redis = Rails.env.production? ? 
-                  { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: 'sidekiq_farmspot' } :
-                  { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: 'sidekiq_farmspot' }
+# Sidekiq::Extensions.enable_delay!
 
+    # if Rails.env.development?
+      Sidekiq.configure_server do |config|
+        config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: "sidekiq_farmspot" }
+      end
+      Sidekiq.configure_client do |config|
+        config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: "sidekiq_farmspot" }
+      end
+    # else
 
-	# config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_farmspot" }
-end
-
-Sidekiq.configure_client do |config|
- config.redis = Rails.env.production? ? 
-                  { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: 'sidekiq_farmspot' } :
-                  { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: 'sidekiq_farmspot' }
-	# config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_farmspot" }
-end
+    # end
+ 
