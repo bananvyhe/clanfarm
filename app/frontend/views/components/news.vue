3100bcffc13f4cc46b05da508829c891809d45f6
bananvyhe <loadonden@yahoo.com>
2024-04-13 03:05:24 +0500

    1

--- app/frontend/views/components/news.vue
+++ app/frontend/views/components/news.vue
@@ -63,7 +63,7 @@
 
 
 
-                <div class="px-0 pt-0 mb-1 colr" :style="[  store.tonline < item.created_at ?  {color: '#9F6B40 !important'}:{} ]">
+                <div class="px-0 pt-0 mb-1 colr" :style="[  store.tonline < item.created_at ?  {color: '#BB9365 !important'}:{} ]">
                   <h3>{{ item.head }}</h3>
                 </div>
                 <div class="px-0 text-body-1 mb-5" :style="[  store.tonline < item.created_at ?  {color: '#959595'}:{} ]">
