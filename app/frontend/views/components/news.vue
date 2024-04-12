231949c5193ccde7d8a0f0de00ddd6b1ee14a75d
bananvyhe <loadonden@yahoo.com>
2024-04-12 21:11:07 +0500

    done

--- app/frontend/views/components/news.vue
+++ app/frontend/views/components/news.vue
@@ -63,10 +63,10 @@
 
 
 
-                <div class="px-0 pt-0 mb-1 colr">
+                <div class="px-0 pt-0 mb-1 colr" :style="[  store.tonline > item.created_at ?  {color: '#D7BA46 !important'}:{} ]">
                   <h3>{{ item.head }}</h3>
                 </div>
-                <div class="px-0 text-body-1 mb-5" :style="[  store.tonline < item.created_at ?  {color: '#999'}:{} ]">
+                <div class="px-0 text-body-1 mb-5" :style="[  store.tonline > item.created_at ?  {color: '#A8A8A8'}:{} ]">
                   {{item.desc}}… 
                 </div>
  <!-- {{store.tonline}} {{item.created_at}} -->
@@ -253,7 +253,7 @@ background-size: cover;
 .colr{
   /*color: #60845b;*/
   /*color: #657d62;*/
-  color: #96ab94;
+  color: #6CA167;
 }
 .date{
   color: #9d9681; 
