f6d8a330d4f28e3b7affb51a05a5602f1e786556
bananvyhe <loadonden@yahoo.com>
2024-05-07 13:10:37 +0500

    back changes login

--- app/frontend/views/components/userbar.vue
+++ app/frontend/views/components/userbar.vue
@@ -6,19 +6,19 @@
  
  <!-- {{route.path}} -->
 
-        <v-btn
+<!--         <v-btn
           v-if="ls.get('gramlog')"
           color="secondary"  
           @click="exitgram"> выйти
-        </v-btn> 
-        <div id="telegram-login-button" class="d-flex align-center mx-2" v-else> </div>
+        </v-btn>  -->
+        <!-- <div id="telegram-login-button" class="d-flex align-center mx-2" v-else> </div> -->
     
     <div v-if="store.tsignedIn == false" class="d-flex"> 
 
     <!-- <p v-if="message">{{ message }}</p> -->
 
-      <!-- <signup></signup> -->
-      <!-- <signin></signin> -->
+      <signup></signup>
+      <signin></signin>
                 <div class="loa px-2 align-self-center" >{{store.tloa}}</div>
           <div class="skull align-self-center"></div>   
     </div>
@@ -123,8 +123,8 @@ const reducedNumber = computed(() => {
   return modifiedNumber;
 });
 
-const exitgram = () => {
-  ls.remove('gramlog')
+// const exitgram = () => {
+  // ls.remove('gramlog')
  //  const telid = ls.get('gramlog').id
  //  plain
  // .post('/signin/exitgram', {  id: telid  })
@@ -133,7 +133,7 @@ const exitgram = () => {
  
  //    })
  //  .catch(error => console.log(error))
-};
+// };
 const ressurect = () => {
     secured
     .post('/users/ressurect')
@@ -173,27 +173,27 @@ const cpimg = new URL("../images/cp/cp.png", import.meta.url).href;
 onMounted(() => {
 const message = ref('');
 
-if(!ls.get('gramlog')){
-  window.onTelegramAuth = (user) => {
-    message.value = "Авторизация прошла успешно!";
-    console.log(message.value)
-    console.log(user)
-    // localStorage.setItem('telegramUserData', JSON.stringify(user));
-    store.setgramlog(user)
-    // sendUserDataToServer(user);
+// if(!ls.get('gramlog')){
+//   window.onTelegramAuth = (user) => {
+//     message.value = "Авторизация прошла успешно!";
+//     console.log(message.value)
+//     console.log(user)
+ 
+//     store.setgramlog(user)
+ 
 
-  };
-    // Инициализация Telegram Login Widget при загрузке компонента
-    const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.setAttribute('data-telegram-login', 'farmspot_bot');
-    script.setAttribute('data-size', 'medium');
-    script.setAttribute('data-radius', '10');
-    script.setAttribute('data-request-access', 'write');
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-    script.async = true;
-    document.getElementById('telegram-login-button').appendChild(script);
-}
+//   };
+//     // Инициализация Telegram Login Widget при загрузке компонента
+//     const script = document.createElement('script');
+//     script.src = 'https://telegram.org/js/telegram-widget.js?22';
+//     script.setAttribute('data-telegram-login', 'farmspot_bot');
+//     script.setAttribute('data-size', 'medium');
+//     script.setAttribute('data-radius', '10');
+//     script.setAttribute('data-request-access', 'write');
+//     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+//     script.async = true;
+//     document.getElementById('telegram-login-button').appendChild(script);
+// }
 
 
   if (store.tsignedIn){
