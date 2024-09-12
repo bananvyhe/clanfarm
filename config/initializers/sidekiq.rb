c20eeb16c71f000de2fd6d55205dd0aff717ba4e
bananvyhe <loadonden@yahoo.com>
2024-09-12 22:44:40 +0500

    koi

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,19 @@
 
 # Sidekiq::Extensions.enable_delay!
 
-    # if Rails.env.development?
+    if Rails.env.development?
       Sidekiq.configure_server do |config|
-        config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: "sidekiq_farmspot" }
+        config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
       end
       Sidekiq.configure_client do |config|
-        config.redis = { url: 'redis://127.0.0.1:6380/sidekiq_farm', namespace: "sidekiq_farmspot" }
+        config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
       end
-    # else
-
-    # end
+    else
+      Sidekiq.configure_server do |config|
+        config.redis = { url: 'redis://127.0.0.1:6380/0', namespace: "sidekiq_farmspot" }
+      end
+      Sidekiq.configure_client do |config|
+        config.redis = { url: 'redis://127.0.0.1:6380/0', namespace: "sidekiq_farmspot" }
+      end
+    end
  
