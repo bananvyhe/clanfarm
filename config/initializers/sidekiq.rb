2413c03070c3874c572801881d9cfeb7b300ff44
bananvyhe <loadonden@yahoo.com>
2024-09-14 01:19:13 +0500

    shed fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,19 @@
 
 # Sidekiq::Extensions.enable_delay!
 
-    if Rails.env.development?
-      Sidekiq.configure_server do |config|
-        config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
-      end
-      Sidekiq.configure_client do |config|
-        config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
-      end
-    else
+    # if Rails.env.development?
+      # Sidekiq.configure_server do |config|
+      #   config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
+      # end
+      # Sidekiq.configure_client do |config|
+      #   config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_farmspot" }
+      # end
+    # else
       Sidekiq.configure_server do |config|
         config.redis = { url: 'redis://127.0.0.1:6380/0', namespace: "sidekiq_farmspot" }
       end
       Sidekiq.configure_client do |config|
         config.redis = { url: 'redis://127.0.0.1:6380/0', namespace: "sidekiq_farmspot" }
       end
-    end
+    # end
  
