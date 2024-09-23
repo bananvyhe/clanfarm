60a2efdd5a2c25cf64a0e95fd45e6a1b675e719d
bananvyhe <loadonden@yahoo.com>
2024-09-24 02:41:23 +0500

    time left calc

--- app/frontend/views/components/scheduler.vue
+++ app/frontend/views/components/scheduler.vue
@@ -27,7 +27,7 @@
 
       <VueDatePicker
       :disabled="item.switchValue == 'вкл' ? true : false"
-        dark locale="ru"  
+        dark locale="ru"
         placeholder="Выбор даты" 
         @closed="alertFn"
         @focus="fillCurrentTime(item.id, item.date)"
@@ -40,11 +40,8 @@
         :modelValue="item.date" 
         model-type="timestamp"/>
 
-
-
         <div v-if="item.id != 0" class="d-flex flex-row align-center interface">
 <!--           <div style="width: 96px;">
-
          </div> -->
  
           <v-text-field 
@@ -56,7 +53,6 @@
             variant="solo-inverted" 
             hide-details 
             clearable>
-              
           </v-text-field>
           <div>
             <v-switch
@@ -71,9 +67,9 @@
             </v-switch> 
           </div>
 
-          <div class="ma-2">
-            
-          3ч:59м
+          <div class="ma-2 animated-time">            
+          <!-- 3ч:59м -->
+          {{ formattedTime(item.date) }}
           </div>
           <v-btn
             @click="removeById(item.id)"
@@ -96,174 +92,168 @@
       </v-btn>    
     </div>
  
-    <div v-if="store.tsignedIn" id="telegram-login-button" class="d-flex align-center 
-
-mx-2" ></div>  
+    <div v-if="store.tsignedIn" id="telegram-login-button" class="d-flex align-center mx-2" ></div>  
     <!-- <div>{{store.tgramlog}}</div> -->
   </div>
-
- 
-
 
 </div> 
 
 </template>
 <script setup lang="ts">
-  import { useLogStore } from '../../store.js'
-  const store = useLogStore()
-  const plain: any = inject('plain')
+import { gsap } from "gsap";
+import { useLogStore } from '../../store.js'
+const store = useLogStore()
+const plain: any = inject('plain')
 
-  const secured: any = inject('secured')
-  const switcher = ref(true)
-  const people = ref(['John'])
-  const telegr = ref('');
-  const customDateFormat = 'dd/MM/yyyy, HH:mm';
-  import { v4 as uuidv4 } from 'uuid';
-  const date = ref([{ id: 0, date: null, switchValue: "выкл", inputValue: null }])
-   
-  const sortedDate = computed(() => {
-    return date.value.slice().sort((a, b) => new Date(a.date) - new Date(b.date));
-  });
+const secured: any = inject('secured')
+const switcher = ref(true)
+const people = ref(['John'])
+const telegr = ref('');
+const customDateFormat = 'dd/MM/yyyy, HH:mm';
+import { v4 as uuidv4 } from 'uuid';
+const date = ref([{ id: 0, date: null, switchValue: "выкл", inputValue: null }])
+ 
+const sortedDate = computed(() => {
+  return date.value.slice().sort((a, b) => new Date(a.date) - new Date(b.date));
+});
 const power = ref(78)
-  const fillCurrentTime = (ind, vremya) => {
-    // const now = new Date().getTime();
-    // date.value[0].date = now;  // Устанавливаем текущее время
- 
-    if (vremya != null) {
-      console.log("vremya esty"+date) 
+const fillCurrentTime = (ind, vremya) => {
+  // const now = new Date().getTime();
+  // date.value[0].date = now;  // Устанавливаем текущее время
 
+  if (vremya != null) {
+    console.log("vremya esty"+date) 
+
+  }else{
+    const now = new Date().getTime();
+    const val = ref(uuidv4());
+
+    if (ind != 0) {
+      const item = date.value.find(d => d.id === ind);
+      // item.date = vremya;
     }else{
-      const now = new Date().getTime();
-      const val = ref(uuidv4());
-
-      if (ind != 0) {
- 
-
-
-        const item = date.value.find(d => d.id === ind);
-        // item.date = vremya;
-
-      }else{
-        const item = date.value.find(d => d.id === ind);
-        item.date = now
-        item.id = val
-      }
-      // date.value.id[ind].date = now
-
-      // console.log(date.value[0].date ) 
-      // console.log("vremya null"+date) 
-      // console.log(item) 
+      const item = date.value.find(d => d.id === ind);
+      item.date = now
+      item.id = val
     }
   }
+}
 
 // Функция для удаления элемента по id
 function removeById(id) {
   date.value = date.value.filter(item => item.id !== id);
   console.log(`Элемент с id ${id} удалён.`);
 }
+const addItem = () => {
+  const val = ref(uuidv4());
+  console.log(val.value) 
 
-// const addItem = () => {
-//   date.value.push('666');
-// };
-// const date = ref(new Date().getTime());
-  const addItem = () => {
-  // date.value.push(new Date().getTime().toString());
-    const val = ref(uuidv4());
-    console.log(val.value) 
+  date.value.push({ id: 0, date: null, switchValue: "выкл", inputValue: null }); 
+  // date.value.push('')
+};
 
-    date.value.push({ id: 0, date: null, switchValue: "выкл", inputValue: null }); 
-    // date.value.push('')
-  };
- 
-   const updateDate = (index, newValue) => {
-    // date.value[index] = newValue;
-    // date.value[index] = date.value[index].value;
+const updateDate = (index, newValue) => {
+// date.value[index] = newValue;
+// date.value[index] = date.value[index].value;
 
-    console.log(date.value) 
-    // const item = date.value[index];
-    const item = date.value.find(d => d.id === index);
-    item.date = newValue
-    // const nomer =  date.value.length
+  console.log(date.value) 
+  // const item = date.value[index];
+  const item = date.value.find(d => d.id === index);
+  item.date = newValue
+  // const nomer =  date.value.length
 
-    console.log(date.length)
-    if (item) {
-      item.date = newValue;
-      // item.id = nomer
-    }
-  
-  };
+  // console.log(date.length)
+  if (item) {
+    item.date = newValue;
+    // item.id = nomer
+  }
 
-
+};
 const vaba = ref("выкл"); 
-// const selectedDate = ref(null);  // Выбранная дата
-// const lastSelectedDate = ref(null);  // Последняя сохраненная дата
-// const selectedTime = ref({hours: null, minutes: null});  // Отдельно храним выбранное время
 const alertFn = () => {
-  // alert('Menu closed');
-  // if (!selectedDate.value) {
-  //   selectedDate.value = lastSelectedDate.value || new Date();  // Используем последнее выбранное время или текущее время
-  //    const newDate = new Date(selectedDate.value);
-  //   newDate.setHours(selectedTime.value.hours);
-  //   newDate.setMinutes(selectedTime.value.minutes);
-  //   selectedDate.value = newDate;
-  // } else {
-  //   lastSelectedDate.value = selectedDate.value;  // Обновляем последнюю выбранную дату
-  // }
-  // lastSelectedDate.value = selectedDate.value;
 } 
 
-  const format = (date) => {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
+const format = (date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
+}
+
+const isButtonDisabled = computed(() => {
+  return !store.tsignedIn || date.value.some(d => d.id === 0);
+});
+
+async function telega() {
+  if(store.tsignedIn){
+// Инициализация Telegram Login Widget при загрузке компонента
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.setAttribute('data-telegram-login', 'farmspot_bot');
+    script.setAttribute('data-size', 'medium');
+    script.setAttribute('data-radius', '10');
+    script.setAttribute('data-request-access', 'write');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.async = true;
+    document.getElementById('telegram-login-button').appendChild(script);
+  }
+}
+
+// Функция для форматирования времени
+const formattedTime = (timestamp) => {
+  // Если timestamp больше, чем 10^10, это миллисекунды — делим на 1000
+  if (timestamp > 1e10) {
+    timestamp = Math.floor(timestamp / 1000); // Приводим к секундам
+  }
+  console.log(timestamp)
+  const now = Math.floor(Date.now() / 1000); // Текущее время в секундах
+  console.log(now)
+  const remainingSeconds = timestamp - now; // Разница между текущим временем и timestamp
+  console.log(remainingSeconds)
+  if (remainingSeconds <= 0) {
+    return 'Время вышло';
   }
 
-  const isButtonDisabled = computed(() => {
-    
-    return !store.tsignedIn || date.value.some(d => d.id === 0);
- 
-  });
 
-  async function telega() {
-    if(store.tsignedIn){
-  // Инициализация Telegram Login Widget при загрузке компонента
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-widget.js?22';
-      script.setAttribute('data-telegram-login', 'farmspot_bot');
-      script.setAttribute('data-size', 'medium');
-      script.setAttribute('data-radius', '10');
-      script.setAttribute('data-request-access', 'write');
-      script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-      script.async = true;
-      document.getElementById('telegram-login-button').appendChild(script);
-    }
+  const days = Math.floor(remainingSeconds / 86400); // 86400 секунд в дне
+  const hours = Math.floor((remainingSeconds % 86400) / 3600);
+  const minutes = Math.floor((remainingSeconds % 3600) / 60);
+  if (days > 0) {
+    return `${days}д:${hours}ч:${minutes}м`; // Если остались дни
   }
+  return `${hours}ч:${minutes}м`;
+};
+
+
 onUpdated(() => {
   console.log("onUpdated")
   telega()
 })
+onUnmounted(() => {
+ 
+});
 onMounted(() => {
-console.log("onMounted")
-    window.onTelegramAuth = (user) => {
-      telegr.value = "Авторизация прошла успешно!";
-      console.log(telegr.value)
-      console.log(user)
-      const sendteleg = () => {
-          secured
-          .post('/users/addtelegram',{id: store.tgramlog.id, username: store.tgramlog.username })
-          .then(response => {
-            console.log(response)
-   
-          })
-        .catch(error => console.log(error))
-      };
-      store.setgramlog(user)
-      sendteleg();
+ 
+  console.log("onMounted")
+
+  window.onTelegramAuth = (user) => {
+    telegr.value = "Авторизация прошла успешно!";
+    console.log(telegr.value)
+    console.log(user)
+    const sendteleg = () => {
+        secured
+        .post('/users/addtelegram',{id: store.tgramlog.id, username: store.tgramlog.username })
+        .then(response => {
+          console.log(response)
+        })
+      .catch(error => console.log(error))
     };
- telega()
+    store.setgramlog(user)
+    sendteleg();
+  };
+  telega()
 })
 // if(!ls.get('gramlog')){
 
@@ -271,13 +261,18 @@ console.log("onMounted")
 </script>
 
 <style scoped  lang="scss" >
-  .swi{
+.animated-time {
+  font-size: 14px;
+  font-weight: bold;
+  display: inline-block;
+}
+.swi{
 
-  }
- .interface{
-  width: 100%;
+}
+.interface{
+width: 100%;
 
- }
+}
 .progresbar{
 
 }
