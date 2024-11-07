71cf2849aa656a52014012eba0c71449773954ff
bananvyhe <loadonden@yahoo.com>
2024-11-07 21:42:57 +0500

    time shedule

--- app/frontend/views/components/daily.vue
+++ app/frontend/views/components/daily.vue
@@ -1,25 +1,98 @@
 <template>
   <div class="schedule">
+
+  <v-item-group multiple>
+    <v-container>
+      
+        <div
+          v-for="n in hours"
+          :key="n"
+          cols="1"
+          class="my-2"
+        >
+          <v-item v-slot="{ isSelected, toggle }">
+            <v-card
+              :color="isSelected ? 'primary' : ''"
+              class="d-flex align-center selectable-item"
+              height="70"
+              dark
+              @mousedown="startSelection(toggle)"
+              @mousemove="onMouseMove(toggle, isSelected)"
+              @mouseup="endSelection"
+            >
+              <v-scroll-y-transition>
+                <div
+                  class="text-h3 flex-grow-1 text-center"
+                >
+                 {{ formatHour(n) }}
+                </div>
+              </v-scroll-y-transition>
+            </v-card>
+          </v-item>
+        </div>
+       
+    </v-container>
+  </v-item-group>
+
+
+
+
     {{hours}}{{schedule}}
-    <div v-for="hour in hours" :key="hour" class="hour-block">
-
+    <div v-for="hour in hours" :key="hour" class="hour-block d-flex">
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
       <div class="hour-label">{{ formatHour(hour) }}</div>
-
-      <div
+        <div
+        v-bind="props"
+        :color="isHovering ? 'primary' : undefined"
         class="schedule-item"
         v-for="(item, index) in schedule[hour]"
         :key="index"
-        @click="removeItem(hour, index)">
-        {{ item.name }}
-      </div>
+        @click="removeItem(hour, index)"
+        @contextmenu.prevent="handleRightClick">
+        {{ item   }}
+        </div>
+      
+        </template>
+      </v-hover>
 
-      <button @click="addItem(hour)">Add</button>
+      <button @click="addItem(hour, index)">Add</button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
- 
+const isSelecting = ref(false);
+
+const startSelection = (toggle) => {
+  isSelecting.value = true;
+  toggle(); // Выделить первый элемент на котором нажата кнопка
+};
+
+const onMouseMove = (toggle, isSelected) => {
+  if (isSelecting.value && !isSelected) {
+    toggle(); // Выделить элемент, если он еще не выбран
+  }
+};
+
+const endSelection = () => {
+  isSelecting.value = false;
+};
+
+// Сброс флага при отпускании кнопки за пределами элементов
+const onGlobalMouseUp = () => {
+  isSelecting.value = false;
+};
+
+onMounted(() => {
+  window.addEventListener('mouseup', onGlobalMouseUp);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('mouseup', onGlobalMouseUp);
+});
+
+
 
 // Определение типа для события
 interface ScheduleItem {
@@ -27,11 +100,15 @@ interface ScheduleItem {
 }
 
 const hours = Array.from({ length: 24 }, (_, i) => i);
-
 // Инициализация schedule с пустыми массивами для каждого часа
 const schedule = ref<{ [key]: ScheduleItem[] }>(
   Object.fromEntries(hours.map(hour => [hour, []]))
 );
+
+const handleRightClick = (event) => {
+  console.log("Right-click detected", event);
+  // Ваш код для обработки клика правой кнопкой
+};
 
 function setItem(hour, index) {
 
@@ -39,11 +116,19 @@ function setItem(hour, index) {
 
 }
 
-function addItem(hour) {
-  const name = prompt("Enter event name:");
-  if (name) {
-    schedule.value[hour].push({ name });
+function addItem(hour, index) {
+  // const name = prompt("Enter event name:");
+  if (schedule.value[hour][0] == null) {
+  console.log(typeof schedule.value[hour])
+  schedule.value[hour].push( true );
+  console.log( schedule.value[hour][0])
+  }else if(typeof schedule.value[hour] === 'boolean'){
+    // schedule.value[hour].push({ tru });
+    // console.log('boolean')
   }
+  // if (name) {
+  //   schedule.value[hour].push({ name });
+  // }
 }
 
 function removeItem(hour: number, index: number) {
@@ -63,6 +148,8 @@ function formatHour(hour: number) {
   margin: auto;
 }
 .hour-block {
+
+/*  background-color: #ada;*/
   display: flex;
   align-items: center;
   margin-bottom: 10px;
